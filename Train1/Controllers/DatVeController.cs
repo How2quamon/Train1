aa609ed8cd7b2703f717094f921d7d7385d71c3e
JohanTT <thejohan39@gmail.com>
2022-12-04 10:29:03 +0700

    Add Api Controller and MVC controller

---
+++ Train1/Controllers/DatVeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Train1.Controllers
+{
+    public class DatVeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("datve");
+        }
+    }
+}
---
+++ Train1/Controllers/VeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Train1.Data;
+using Train1.Models;
+
+namespace Train1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeController : ControllerBase
+    {
+        private readonly CTQM_DbContext _context;
+
+        public VeController(CTQM_DbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return ViewResult("datve");
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var dsVe = _context.Ves.ToList();
+            return Ok(dsVe);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var Ve = _context.Ves.SingleOrDefault(v => v.MaVe == id);
+            if (Ve != null)
+            {
+                return Ok(Ve);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        /*
+                [HttpPost]
+                public IActionResult CreateNew(VeViewModel model)
+                {
+                    var Ve = new Ve
+                    {
+
+                    };
+                    _context.Add(model);
+                    _context.SaveChanges();
+
+                }
+        */
+    }
+}
